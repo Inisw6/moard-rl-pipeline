52e376dbd39e92daf3d0082ef92f0f14af93307e
ChaneHaDa <chane.ha.da@gmail.com>
2025-05-28 15:55:35 +0900

    feat: 컨벤션 적용

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -2,11 +2,14 @@ from abc import ABC, abstractmethod
 
 class BaseAgent(ABC):
     @abstractmethod
-    def select_action(self, state): ...
+    def select_action(self, state):
+        pass
     @abstractmethod
-    def store(self, *args): ...
+    def store(self, *args):
+        pass
     @abstractmethod
-    def learn(self): ...
+    def learn(self):
+        pass
     @abstractmethod
     def save(self, path: str):
         raise NotImplementedError
